4a70e4c4a5ca6f3d75904fb88ace486d396d694d
nickgiann <nickgiann@outlook.com>
2021-01-11 20:43:48 +0200

    Fixed something that we previously broke

--- The Grand Canyon Bomber/GameObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "gameobject.h"
-#include "game.h"
-
-GameObject::GameObject(const Game& mygame)
-	:game(mygame)
-{
-}
--- The Grand Canyon Bomber/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ASSET_PATH "asset\\"
-#define WINDOW_WIDTH 1200
-#define WINDOW_HEIGHT 600
-#define CANVAS_WIDTH 1000
-#define CANVAS_HEIGHT 500
